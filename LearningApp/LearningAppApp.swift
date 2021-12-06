4ab7692f5a46eaf3c0458e6c0b6c96d287083ed2
Mullen44 <65976580+Mullen44@users.noreply.github.com>
2021-12-06 14:23:51 -0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Andrew Mullen on 2021-12-06.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
